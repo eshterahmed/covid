8164fc87e8c24522fa208afabf3c9bd62ed5447a
eshterahmed <mail@eshterahmed.com>
2020-03-25 02:55:52 -0500

    updated on march 25th

--- js/demo/chart-area-demo.js
+++ js/demo/chart-area-demo.js
@@ -173,7 +173,7 @@ var myLineChart = new Chart(ctx, {
       data: [3,3,3,3,3,3, 5, 8, 10, 14, 17, 20, 25, 27,33, Activecases, Activecases + 5 ],
     },
     {
-      data:[0,0,0,0,0,0,0,0,1,1,2,2,2,3,3,4],
+      data:[0,0,0,0,0,0,0,1,1,2,2,2,3,3,4,5],
       label: "Deaths ",
       lineTension: 0.2,
       backgroundColor: "rgba(255, 99, 71, 0.5)",
