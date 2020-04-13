279acc3bb5f58a15778fd7e1971c0eb91f9ab86e
eshterahmed <mail@eshterahmed.com>
2020-04-13 14:58:02 -0500

    up3

--- js/demo/chart-area-demo.js
+++ js/demo/chart-area-demo.js
@@ -170,7 +170,7 @@ var myLineChart = new Chart(ctx, {
       pointHoverBorderColor: "rgb(179, 0, 0)",
       pointHitRadius: 10,
       pointBorderWidth: 2,
-      data: [39,44,48,48,49,51,54,56,70,88, 123,164,218,330,424,482,621,Activecases, Activecases + 10],
+      data: [48,48,49,51,54,56,70,88, 123,164,218,330,424,482,621,Activecases, Activecases + 10],
     },
     {
       data:[5,5,5,6,6,6,8,9,12,17,20,21,27,30,34,39],
