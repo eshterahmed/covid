7462b5bcecfea48e8c32084e5b2d158c7fe76ecf
eshterahmed <eshterahmed@gmail.com>
2020-04-20 03:55:12 -0500

    update 20 april

--- js/demo/chart-area-demo.js
+++ js/demo/chart-area-demo.js
@@ -149,7 +149,7 @@ p15 = p15.format("l");
 var now = moment();
 now = now.format("l");
 
-var Activecases = 2456;
+var Activecases = 2948;
 
 var ctx = document.getElementById("myAreaChart");
 
@@ -170,10 +170,10 @@ var myLineChart = new Chart(ctx, {
       pointHoverBorderColor: "rgb(179, 0, 0)",
       pointHitRadius: 10,
       pointBorderWidth: 2,
-      data: [70,88, 123,164,218,330,424,482,621,803,1012,1231,1572,1838,2144,Activecases, Activecases + 10],
+      data: [88, 123,164,218,330,424,482,621,803,1012,1231,1572,1838,2144,2456,Activecases, Activecases + 10],
     },
     {
-      data:[8,9,12,17,20,21,27,30,34,39,46,50,60,75,84,91],
+      data:[9,12,17,20,21,27,30,34,39,46,50,60,75,84,91,101],
       label: "Deaths ",
       lineTension: 0.2,
       backgroundColor: "rgba(255, 99, 71, 0.5)",
